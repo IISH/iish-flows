28546d1d3f190fee991408d444d9d55fb9207e2d
flow application server <flow@dis-app-iish-00.iisg.net>
2013-06-12 17:18:18 +0200

    Add procedure for checking flow shares

--- src/main/flow1/startup.sh
+++ src/main/flow1/startup.sh
@@ -6,29 +6,34 @@
 
 source $FLOWS_HOME/config.sh
 
-for project in $FLOW1_SHARE/*
+net use $FLOW1_SHARE
+
+for flow_share in $flow1_sharenames
 do
-    for d in $project/*
-    do
-        if [ -d $d ] ; then
-            na=$(basename $d)
-            for fileSet in $d/*
-            do
-                if [ -d $fileSet ] ; then
-                    if [ -f $fileSet/validate.txt ] || [ -f $fileSet/valideer.txt ] ; then
-                        rm -f $fileSet/vali*
-                        $flow2_home/validate.sh -na $na -fileSet $fileSet
-                    fi
-                    if [ -f $fileSet/ingest.txt ] ; then
-                        rm -f $fileSet/ingest.txt
-                        $flow2_home/ingest.sh -na $na -fileSet $fileSet
-                    fi
-                    if [ -f $fileSet/checksum.txt ] ; then
-                        rm -f $fileSet/checksum.txt
-                        $flow2_home/checksum.sh -na $na -fileSet $fileSet
-                    fi
-                fi
-            done
-        fi
-    done
+	for project in $FLOW1_SHARE/$flow_share
+	do
+		for d in $project/*
+		do
+			if [ -d $d ] ; then
+				na=$(basename $d)
+				for fileSet in $d/*
+				do
+					if [ -d $fileSet ] ; then
+						if [ -f $fileSet/validate.txt ] || [ -f $fileSet/valideer.txt ] ; then
+							rm -f $fileSet/vali*
+							$flow1_home/validate.sh -na $na -fileSet $fileSet
+						fi
+						if [ -f $fileSet/ingest.txt ] ; then
+							rm -f $fileSet/ingest.txt
+							$flow1_home/ingest.sh -na $na -fileSet $fileSet
+						fi
+						if [ -f $fileSet/checksum.txt ] ; then
+							rm -f $fileSet/checksum.txt
+							$flow1_home/checksum.sh -na $na -fileSet $fileSet
+						fi
+					fi
+				done
+			fi
+		done
+	done
 done
