18d90fb20cb8adb47e58e95a57d7223d5dca24f8
flow application server <flow@dis-app-iish-00.iisg.net>
2013-07-19 20:22:09 +0200

    Add checksum to prevent different versions of csv files to play a part in the ingest

--- src/main/flow1/validate/run.sh
+++ src/main/flow1/validate/run.sh
@@ -13,6 +13,7 @@ report=$work/report.txt
 echo "Start validation">>$log
 echo "Validation for $archiveID" > $report
 echo "Started on $(date)">>$report
+md5sum $fileSet/$archiveID.csv > $work/$archiveID.csv.md5
 java -Xms512m -Xmx512m -cp $(cygpath --windows "$HOMEPATH\.m2\repository\org\objectrepository\validation\1.0\validation-1.0.jar") org.objectrepository.validation.ConcordanceMain -fileSet $(cygpath --windows "$fileSet") >> $report
 cf=$work/concordanceValidWithPID.csv
 mv $fileSet/concordanceValidWithPID.csv $cf
@@ -25,8 +26,6 @@ fi
 echo "You can savely ignore warnings about Thumbs.db" >> $report
 echo $(date)>>$log
 echo "Done validate.">>$log
-
-md5sum $fileSet/$archiveID.csv > $work/$archiveID.csv.md5
 
 body="/tmp/report.txt"
 echo "Rapportage op $report">$body
