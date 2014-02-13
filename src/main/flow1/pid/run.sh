2be8afc3157e0c89fc0ef140b7e586e957d8b0dc
flow application server <flow@dis-app-iish-00.iisg.net>
2014-02-13 18:26:34 +0100

    Parse pid response output to /dev/null

--- src/main/flow1/pid/run.sh
+++ src/main/flow1/pid/run.sh
@@ -21,7 +21,6 @@ echo "Declare pids...">>$log
 # Example line is
 # 1,1,/ARCH00518/Tiff/1/1_0005.tif,/ARCH00518/Jpeg/1/1_0005.jpg,5,10622/A3EF7419-A1D8-4698-8369-F62ADAEC703E
 lastpid=""
-file=$work/pid.log
 while read line
 do
     while IFS=, read objnr ID master jpeg volgnr PID
@@ -51,19 +50,17 @@ do
 					</soapenv:Body> \
 				</soapenv:Envelope>"
 
-			echo "Sending $soapenv" >> $log
-			wget -O $file --header="Content-Type: text/xml" \
+			
+			echo "Sending $objid" >> $log
+			wget -O /dev/null --header="Content-Type: text/xml" \
 				--header="Authorization: oauth $pidwebserviceKey" --post-data "$soapenv" \
 				--no-check-certificate $pidwebserviceEndpoint
 
-			pidCheck=$(php $global_home/pid.php -l $file)
-			if [ "$pidCheck" == "$objid" ] ; then
-				rm $file
-				echo "Pid ${objid} bind ok">>$log
-			else
-				echo "ERROR: Pid ${objid} not returned by webservice. Got ${pidCheck}">>$log
+			rc=$?
+			if [[ $rc != 0 ]]; then
+				echo "Error from PID webservice: $rc">>$log
+				echo $soapenv >> $log
 				cat $file >> $log
-				exit -1
 			fi
         fi
     done
@@ -94,19 +91,15 @@ done < $cf
 			</soapenv:Body> \
 		</soapenv:Envelope>"
 	
-	echo "Sending $soapenv" >> $log
-	wget -O $file --header="Content-Type: text/xml" \
+	echo "Sending $pid" >> $log
+	wget -O /dev/null --header="Content-Type: text/xml" \
         --header="Authorization: oauth $pidwebserviceKey" --post-data "$soapenv" \
         --no-check-certificate $pidwebserviceEndpoint
 
-    pidCheck=$(php $global_home/pid.php -l $file)
-    if [ "${pidCheck^^}" != "${pid^^}" ] ; then
-        echo "ERROR: Pid ${pid} not returned by webservice. Got ${pidCheck}">>$log
-		cat $file >> $log
-		exit -1
-	else
-		rm $file
-		echo "Pid ${pid} bind ok">>$log
-    fi
+    rc=$?
+	if [[ $rc != 0 ]]; then
+		echo "Error from PID webservice: $rc">>$log
+		echo $soapenv >> $log
+	fi
 
 echo "Done file pid declarations.">>$log
