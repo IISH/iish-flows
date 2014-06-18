c38880734db996e351ff2a9bb15ea8d4d16b744a
Lucien van Wouw <lwo@iisg.nl>
2014-06-18 13:01:07 +0200

    Create a definition for a new flow5 upload

---
+++ src/main/flow5/create/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# flow5/create.sh
+#
+#
+# Create a folder with the given datestamp and an ingest command.
+
+if [ -z "$datestamp" ] ; then
+    echo "No datestamp was set." >> $log
+    exit -1
+fi
+
+# Remember we are in a fileset: .../create_flow5_commands/
+# We need to step back to the parent and set /datestamp/
+fileSet=$fs_parent/$datestamp
+instruction=$fileSet/instruction.xml
+if [ -f $instruction ] ; then
+    echo "Instruction already created: ${instruction}" >> $log
+    exit -1
+fi
+
+mkdir $fileSet
+touch $fileSet/ingest.txt
+
+exit 0
