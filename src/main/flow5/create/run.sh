23ab96c0a4924cd8c5b64e6ecaeff542437d8153
flow application server <flow@dis-app-iish-00.iisg.net>
2014-07-03 11:06:22 +0200

    Ensure no other jobs start

--- src/main/flow5/create/run.sh
+++ src/main/flow5/create/run.sh
@@ -14,17 +14,33 @@ if [ -z "$datestamp" ] ; then
     exit -1
 fi
 
-# Remember we are in a fileset: .../create_flow5_commands/
-# We need to step back to the parent and set /datestamp/
-fileSet=$fs_parent/$datestamp
-instruction=$fileSet/instruction.xml
-if [ -f $instruction ] ; then
-    echo "Instruction already created: ${instruction}" >> $log
-    exit -1
+# We will not make fileSets whilst others are still running.
+# That is, there should only be:
+# one folder and a slot is available
+# two folders and the slot is in use. We skip the procedure.
+count=$(ls $fs_parent | wc -l)
+if [[ $count == 0 ]] ; then
+	echo "There is no System folder." >> $log
+	mkdir $fs_parent/DO_NOT_REMOVE_THIS_FOLDER
+	touch $fs_parent/DO_NOT_REMOVE_THIS_FOLDER/system
 fi
 
-echo "Creating ${fileSet}" >> $log
-mkdir $fileSet
-touch $fileSet/ingest.txt
+if [[ $count == 1 ]] ; then
+	# Remember we are in a fileset: .../create_flow5_commands/
+	# We need to step back to the parent and set /datestamp/
+	fileSet=$fs_parent/$datestamp
+	instruction=$fileSet/instruction.xml
+	if [ -f $instruction ] ; then
+		echo "Instruction already created: ${instruction}" >> $log
+		exit -1
+	fi
 
-exit 0
+	echo "Creating ${fileSet}" >> $log
+	mkdir $fileSet
+	touch $fileSet/ingest.txt
+
+	exit 0
+fi
+
+echo "Slot in use." >> $log
+exit 1
