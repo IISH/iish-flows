4d5017b8f83fb6f73c69c239e02d5a5408b3d3ee
Lucien van Wouw <lwo@iisg.nl>
2016-02-19 13:02:04 +0100

    Parse problems to log

--- src/main/flow1/barcode/run.sh
+++ src/main/flow1/barcode/run.sh
@@ -9,7 +9,7 @@
 source $FLOWS_HOME/src/main/global/setup.sh $0 "$@"
 
 echo "Check concordance for missing masters" >> $log
-python concordance_missing_masters.py --fileset "${fileSet}" --concordance "${fileSet}/$archiveID.bad.csv" --new "${fileSet}/$archiveID.good.csv"
+python concordance_missing_masters.py --fileset "${fileSet}" --concordance "${fileSet}/$archiveID.bad.csv" --new "${fileSet}/$archiveID.good.csv" >> $log
 rc=$?
 if [[ $rc != 0 ]] ; then
 	echo "Problem with creating new masters." >> $log
