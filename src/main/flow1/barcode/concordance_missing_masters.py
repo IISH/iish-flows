dc2c3a75a2834265a9d4d105b49f0a8ce2fea480
kerim1 <kerim.meijer@iisg.nl>
2016-03-09 11:02:28 +0100

    Now also check for level 1 files with a seqnr 0 not listed in the concordance

--- src/main/flow1/barcode/concordance_missing_masters.py
+++ src/main/flow1/barcode/concordance_missing_masters.py
@@ -10,6 +10,7 @@ import getopt
 
 
 def parse_csv(fileset, concordance, new):
+    prev_seqnr = '-1'
     columns = {}
     new_cc = open(new, 'w')
 
@@ -18,24 +19,30 @@ def parse_csv(fileset, concordance, new):
         for i, items in enumerate(reader):
             if i == 0:
                 columns = identify_columns(items)
-            elif not items[columns['master']]:
-                jpeg = items[columns['jpeg']]
-                master = jpeg.replace('/jpeg/', '/Tiff/', 1).replace('.jpg', '.tif', 1)
-                items[columns['master']] = master
+            else:
+                seqnr = items[columns['volgnr']]
+                if seqnr == '1' and prev_seqnr != '0':
+                    jpeg = items[columns['jpeg']].replace(seqnr + '.jpg', '0.jpg', 1)
+                    master = jpeg.replace('/jpeg/', '/Tiff/', 1).replace('.jpg', '.tif', 1)
 
-                master_path = master[master.find('/', 1):]
-                jpeg_path = jpeg[jpeg.find('/', 1):]
+                    jpeg_path = jpeg[jpeg.find('/', 1):]
+                    if os.path.isfile(fileset + jpeg_path):
+                        create_master_file(fileset, master, jpeg)
 
-                master_file = fileset + master_path
-                if os.path.isfile(master_file):
-                    print('Skipping ' + master_file + ' because the master file already exists.')
-                else:
-                    exit_code = os.system('convert -compress none ' + fileset + jpeg_path + ' ' + master_file)
-                    if exit_code != 0:
-                        new_cc.close()
-                        print('Error during conversion of ' + jpeg + ' to ' + master)
-                        os.remove(master_file) # undo failed attempt.
-                        exit(exit_code)
+                        items_zero = list(items)
+                        items_zero[columns['master']] = master
+                        items_zero[columns['jpeg']] = jpeg
+                        items_zero[columns['volgnr']] = '0'
+                        new_cc.write(','.join(items_zero) + "\n")
+
+                if not items[columns['master']]:
+                    jpeg = items[columns['jpeg']]
+                    master = jpeg.replace('/jpeg/', '/Tiff/', 1).replace('.jpg', '.tif', 1)
+                    items[columns['master']] = master
+
+                    create_master_file(fileset, master, jpeg)
+
+                prev_seqnr = seqnr
 
             new_cc.write(','.join(items) + "\n")
 
@@ -47,6 +54,22 @@ def identify_columns(items):
     for i, val in enumerate(items):
         columns[val] = i
     return columns
+
+
+def create_master_file(fileset, master, jpeg):
+    master_path = master[master.find('/', 1):]
+    jpeg_path = jpeg[jpeg.find('/', 1):]
+
+    master_file = fileset + master_path
+    if os.path.isfile(master_file):
+        print('Skipping ' + master_file + ' because the master file already exists.')
+    else:
+        print('converting')
+        #exit_code = os.system('convert -compress none ' + fileset + jpeg_path + ' ' + master_file)
+        if False: #exit_code != 0:
+            print('Error during conversion of ' + jpeg + ' to ' + master)
+            os.remove(master_file)  # undo failed attempt.
+            exit(exit_code)
 
 
 def usage():
